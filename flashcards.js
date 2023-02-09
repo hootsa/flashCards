2f644450f0d743859b2a1c3c6d6463f6a5c6e0af
Hootsa <hootsa.kh@gmail.com>
2023-02-09 12:47:55 -0800

    refactor js fu

--- flashcards.js
+++ flashcards.js
@@ -4,11 +4,6 @@ let questions = [
   "What are the first three words of the Constitution?",
   "What is an amendment?",
   "What do we call the first ten amendments to the Constitution?",
-  "What is one right or freedom from the first amendment?",
-  "How many amendments does the Constitution have?",
-  "What did the Declaration of Independence do?",
-  "What are three rights in the Declaration of Independence?",
-  "What is freedom of religion?",
 ];
 let answers = [
   "The Constitution",
@@ -16,62 +11,70 @@ let answers = [
   "We The People",
   "a change/addition (to the constitution)",
   "The Bill of Rights",
-  "Speech",
-  "27",
-  "Declared our Independence",
-  "Life, Liberty, and the Pursuit of Happiness",
-  "you can practice any religion or not practice a religion",
 ];
 
-// get elements
-const entireBorderElement = document.getElementById("entireBorder");
-const displayElement = document.getElementById("display");
-const previousBtnElement = document.getElementById("previousBtn");
-const nextBtnElement = document.getElementById("nextBtn");
-const titleElement = document.getElementById("title");
+//get elements
+
+function GetElement(idName) {
+  const element = document.getElementById(idName);
+  return element;
+}
+
+const entireBorderElement = GetElement("entireBorder");
+const displayElement = GetElement("display");
+const previousBtnElement = GetElement("previousBtn");
+const nextBtnElement = GetElement("nextBtn");
+const titleElement = GetElement("title");
+const quantitiesEl = GetElement("quantities");
+titleElement.classList.add("blue-color");
+//Deafult page load
+
+let isQuestion = true;
+let currentIdx = 0;
+printData();
 
 // Add eventListener
 displayElement.addEventListener("click", flip);
-displayElement.innerHTML = questions[0];
-titleElement.innerHTML = "Question";
-
-let isQuestion = true;
-
-function flip() {
-  console.log(titleElement.innerHTML);
-  if (isQuestion) {
-    isQuestion = false;
-    displayElement.innerHTML = answers[0];
-    titleElement.innerHTML = "Answer";
-  } else {
-    isQuestion = true;
-    displayElement.innerHTML = questions[0];
-    titleElement.innerHTML = "Question";
-  }
-}
-
 nextBtnElement.addEventListener("click", next);
 previousBtnElement.addEventListener("click", previous);
 
-let currentIndexDisply = 0;
-let i = 0;
-
-function next() {
-  if (currentIndexDisply !== questions.length - 1) {
-    i++;
-    displayElement.innerHTML = questions[i];
-    currentIndexDisply++;
+function flip() {
+  if (isQuestion) {
+    isQuestion = false;
+    printData();
   } else {
-    currentIndexDisply = 0;
+    isQuestion = true;
+    printData();
   }
 }
 
-function previous() {
-  if (currentIndexDisply !== questions.length - 1) {
-    i--;
-    displayElement.innerHTML = questions[i];
-    currentIndexDisply--;
-  } else {
-    currentIndexDisply = 0;
+function next() {
+  if (currentIdx >= questions.length - 1) {
+    return;
   }
+  currentIdx++;
+  printData();
+}
+
+function previous() {
+  if (currentIdx === 0) {
+    return;
+  }
+  currentIdx--;
+  printData();
+}
+
+function printData() {
+  if (isQuestion) {
+    displayElement.innerHTML = questions[currentIdx];
+    titleElement.innerHTML = "Question";
+    titleElement.classList.add("blue-color");
+  } else {
+    displayElement.innerHTML = answers[currentIdx];
+    titleElement.innerHTML = "Answer";
+    titleElement.classList.remove("blue-color");
+    titleElement.classList.add("green-color");
+  }
+
+  quantitiesEl.innerHTML = currentIdx + 1 + "/" + questions.length;
 }
